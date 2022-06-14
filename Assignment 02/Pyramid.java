fd2f1dfce40f3dc67c1df5a1deb97d912880ce42
Anas Mannaa <30526159+anasmannaa@users.noreply.github.com>
2022-06-13 23:26:32 -0500

    Create Pyramid.java

---
+++ Assignment 02/Pyramid.java
@@ -0,0 +1,37 @@
+// Stanford 106A
+// Assignment 01 - Problem 03 
+// https://see.stanford.edu/materials/icspmcs106a/13-assignment-2-simple-java.pdf
+
+import acm.graphics.*;
+import acm.program.*;
+import java.awt.*;
+
+public class Pyramid extends GraphicsProgram {
+
+/** Width of each brick in pixels */
+	private static final int BRICK_WIDTH = 30;
+
+/** Width of each brick in pixels */
+	private static final int BRICK_HEIGHT = 12;
+
+/** Number of bricks in the base of the pyramid */
+	private static final int BRICKS_IN_BASE = 14;
+	
+	public void run() {
+		drawPyramid();
+	}
+	
+	private void drawPyramid() {
+		int remainRows = BRICKS_IN_BASE;
+		int rowGab = BRICK_WIDTH / 2;
+		double xStart = (getWidth() / 2) - (BRICK_WIDTH * (BRICKS_IN_BASE/2));
+		double yStart = getHeight() + BRICK_HEIGHT;
+		while ( remainRows > 0) {
+			for (int i = 0; i < remainRows; i++) {
+				GRect rect = new GRect(((BRICKS_IN_BASE - remainRows) * rowGab) + xStart + (i * BRICK_WIDTH), getHeight() - (BRICKS_IN_BASE - remainRows) * BRICK_HEIGHT,  BRICK_WIDTH, BRICK_HEIGHT);
+				add(rect);
+			}
+			remainRows -=1;
+		}
+	}
+}
