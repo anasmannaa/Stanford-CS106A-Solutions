4db90f6933c9ba0bd2da05e24e019f08dd6ee817
Anas Mannaa <30526159+anasmannaa@users.noreply.github.com>
2022-08-01 17:37:42 -0500

    Create Hangman.java

---
+++ Assignment 04/Graphics Version/Hangman.java
@@ -0,0 +1,138 @@
+/*
+ * File: Hangman.java
+ * ------------------
+ * This program will eventually play the Hangman game from
+ * Assignment #4.
+ */
+
+import acm.graphics.*;
+import acm.program.*;
+import acm.util.*;
+
+import java.awt.*;
+
+public class Hangman extends ConsoleProgram {
+	
+	/* Constants for the game */
+	private static final int MAX_TEMPS = 8;
+	
+	/* initializes the canvas and adds it to the
+     * window prior to the run method being executed; 
+     */
+    public void init() {
+    	canvas = new HangmanCanvas();
+    	add(canvas);
+    }
+    
+    /* The run method starts the excution of the game */
+    public void run() {
+    	setUp();
+    	printWelcome();
+    	playTheGame();	
+	}
+    
+    private void setUp() {
+    	word = getNewWord();
+    	dashWord = getDashWord(word);
+    	numGuesses = MAX_TEMPS;
+    	canvas.reset();
+    }
+    
+    private void printWelcome() {
+    	println("Welcome to Hangman!");
+    }
+    
+    private void playTheGame() {
+
+    	while (numGuesses >= 0) {
+    		if (numGuesses == 0) {
+        		CallLoser();
+        		break;
+        	}
+    		if (dashWord.equals(word)) {
+    			printWin();
+    			break;
+    		}
+    		giveNewTry();
+    	}
+    }
+    
+    private void giveNewTry() {
+    	String guessed;
+    	println("The word now looks like this: " + dashWord);
+    	println("You have " + numGuesses + " left.");
+    	guessed = readLine("Your Guess: ").toUpperCase();
+    	if(word.contains(guessed)) {
+    		callRight(guessed);
+    	} else {
+    		callWrong(guessed);
+    	}
+    }
+    
+    private void printWin() {
+    	println("You guessed the word: " + word);
+    	println("You Win!");
+    }
+    
+    private void callRight(String guess) {
+    	if(!charIncluded(guess)) {
+    		includeChar(guess);
+    		if(!matchWord()) {
+    			println("That guess is correct");
+    		}
+    	}
+    }
+    
+    private void callWrong(String guess) {
+    	numGuesses -= 1;
+    	println("There are no " + guess + "'s in the word");
+    	canvas.noteIncorrectGuess();
+    }
+    
+    private void CallLoser() {
+    	println("You are completely hung");
+    	println("The word was: " + word);
+    	println("You lose.");
+    }
+    
+    private boolean charIncluded(String guess) {
+    	return (dashWord.contains(guess));
+    }
+    
+    private void includeChar(String guess) {
+    	String newDashWord = "";
+    	for (int i = 0; i < word.length(); i++) {
+    		if (word.charAt(i) == guess.charAt(0)) {
+    			newDashWord += guess;
+    		} else {
+    			newDashWord += dashWord.charAt(i);
+    		}
+    	}
+    	dashWord = newDashWord;
+    }
+    
+    private boolean matchWord() {
+    	return dashWord == word;
+    }
+    
+    private String getNewWord() {
+    	HangmanLexicon lexicon = new HangmanLexicon();
+    	return lexicon.getWord(rgen.nextInt(lexicon.getWordCount())).toUpperCase();
+    }
+    
+    private String getDashWord(String word) {
+    	String dashWord = "";
+    	for (int i = 0; i < word.length(); i++) {
+    		dashWord = dashWord + "-";
+    	}
+    	return dashWord;
+    }
+    
+    private String word;
+    private String dashWord;
+    private int numGuesses;
+    private RandomGenerator rgen = RandomGenerator.getInstance();
+    private HangmanLexicon lexicon;
+    private HangmanCanvas canvas;
+
+}
