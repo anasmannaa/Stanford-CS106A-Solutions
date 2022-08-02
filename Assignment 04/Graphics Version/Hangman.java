f86651f73336a7280ab1e68230ee78c3248657d7
Anas Mannaa <30526159+anasmannaa@users.noreply.github.com>
2022-08-01 20:20:54 -0500

    Update Hangman.java

--- Assignment 04/Graphics Version/Hangman.java
+++ Assignment 04/Graphics Version/Hangman.java
@@ -31,6 +31,7 @@ public class Hangman extends ConsoleProgram {
     	playTheGame();	
 	}
     
+    /* setup the game console and canvas */
     private void setUp() {
     	word = getNewWord();
     	dashWord = getDashWord(word);
@@ -38,12 +39,16 @@ public class Hangman extends ConsoleProgram {
     	canvas.reset();
     }
     
+    /* prints welcome screen on console at the game start */
     private void printWelcome() {
     	println("Welcome to Hangman!");
     }
     
+    /* plays the game and watch the user guesses and checks
+     * to see if user guessed the right character, gives new try if user
+     * is not out of tries, prints win if user guessed the right word in full,
+     * and prints lose if user out of tries */
     private void playTheGame() {
-
     	while (numGuesses >= 0) {
     		if (numGuesses == 0) {
         		CallLoser();
@@ -74,6 +79,7 @@ public class Hangman extends ConsoleProgram {
     	println("You Win!");
     }
     
+    /* called if user guesses is right */
     private void callRight(String guess) {
     	if(!charIncluded(guess)) {
     		includeChar(guess);
@@ -83,22 +89,26 @@ public class Hangman extends ConsoleProgram {
     	}
     }
     
+    /* called in wrong guess, reduces number of remained tries by one */
     private void callWrong(String guess) {
     	numGuesses -= 1;
     	println("There are no " + guess + "'s in the word");
     	canvas.noteIncorrectGuess();
     }
     
+    /* called if user is out of all tries */
     private void CallLoser() {
     	println("You are completely hung");
     	println("The word was: " + word);
     	println("You lose.");
     }
     
+    /* checks if user guess is right */
     private boolean charIncluded(String guess) {
     	return (dashWord.contains(guess));
     }
     
+    /* replaces the dashes equivalent to right guess */
     private void includeChar(String guess) {
     	String newDashWord = "";
     	for (int i = 0; i < word.length(); i++) {
@@ -111,15 +121,18 @@ public class Hangman extends ConsoleProgram {
     	dashWord = newDashWord;
     }
     
+    /* check if user guessed the right word in full */
     private boolean matchWord() {
     	return dashWord == word;
     }
     
+    /* gets the new guess word of the game turn */
     private String getNewWord() {
     	HangmanLexicon lexicon = new HangmanLexicon();
     	return lexicon.getWord(rgen.nextInt(lexicon.getWordCount())).toUpperCase();
     }
     
+    /* build the equivalent dashes for the guess word */
     private String getDashWord(String word) {
     	String dashWord = "";
     	for (int i = 0; i < word.length(); i++) {
